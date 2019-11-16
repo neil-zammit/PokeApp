ac4c47bdc86963ab325805adb0e09df7606bb363
Neil Zammit <nz3001@gmail.com>
2019-11-16 13:44:42 +0100

    Add getPokeList class method

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +9,7 @@ import { stringify } from 'querystring';
 export class ListComponent implements OnInit {
   // Implement form where user enters how much pokemon will be retrieved
   pokecount = 5;
+
   // class property to hold results
   pokelist = [];
 
@@ -17,10 +17,27 @@ export class ListComponent implements OnInit {
   constructor(private http: HttpService) {}
 
   ngOnInit() {
-    // Loop for pokecount times +1 since no pokemon at index 0 in API db
-    for (let i = 1; i < this.pokecount + 1; i++) {
+    // Loop for pokecount times
+    for (let i = 0; i < this.pokecount; i++) {
+      // Get data from Api
       this.http.getPokemon().subscribe(data => {
-        // console.log(data.name);
+        // Push pokemon name from PokeApi db to pokelist array
+        this.pokelist.push(data.name);
+      });
+    }
+    console.log(this.pokelist);
+  }
+
+  // Fix this method
+  getPokeList() {
+    // Clear pokelist
+    this.pokelist = [];
+    // console.log(this.pokecount);
+    // Loop for pokecount times
+    for (let i = 0; i < this.pokecount; i++) {
+      console.log(this.pokecount);
+      // Get data from Api
+      this.http.getPokemon().subscribe(data => {
         // Push pokemon name from PokeApi db to pokelist array
         this.pokelist.push(data.name);
       });
